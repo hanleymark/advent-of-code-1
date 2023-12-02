71921580a83dfd67824930b3ce9870a4cd71e7b2
hanleymark <home@markhanley.co.uk>
2023-12-02 10:17:19 +0000

    Set up index.js run file with args and tests

--- day-01/test.js
+++ day-01/test.js
@@ -1,13 +1,17 @@
 const assert = require('assert');
-const solution2 = require('./solution2');
+const solution = require('./part2');
 
-describe('Day 1: It should get the first and last digits from a string and make a two digit number', () => {
+describe('Day 1: It should get the first and last digits from a string and make the correct total', () => {
   it('should get digits when >1 are present', () => {
-    assert.strictEqual(solution2('2SGH4JHG5'), '25');
-    assert.strictEqual(solution2('DF34D'), '34');
+    assert.strictEqual(solution('2SGH4JHG5'), 25);
+    assert.strictEqual(solution('DF34D'), 34);
   });
   it('should get double digits when 1 IS present', () => {
-    assert.strictEqual(solution2('4'), '44');
-    assert.strictEqual(solution2('FHSKRH7KHF'), '77');
+    assert.strictEqual(solution('4'), 44);
+    assert.strictEqual(solution('FHSKRH7KHF'), 77);
+  });
+  it('should convert words to digits and get the correct total', () => {
+    assert.strictEqual(solution('3onehdhcknine'), 39);
+    assert.strictEqual(solution('eight5gckh3jdnine'), 89);
   });
 });
